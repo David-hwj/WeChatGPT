fd340ea909d77b8469f60183b9922bbac8aafb6e
David <1134638172@qq.com>
2023-02-15 00:28:31 +0800

    change

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# 启动ChatGPT service
+pid=`ps -ef|grep conversionService.ts|grep loader.js|awk '{print $2}' `  
+if [ "${pid}" ]; then
+    kill -9 ${pid}
+fi
+cd $(dirname "$0")
+cd ChatGPT/node-api
+npm install
+npx tsx conversionService.ts&
+
+
+# 启动Client
+cd ../../Client
+python -m venv .venv
+
+source .venv/bin/activate
+
+python -m pip install -r requirements.txt
+python client.py
